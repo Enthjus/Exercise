5255b08516341f2d5b87d087b72937a4acdc6bf1
Nguyen Dinh Phuc <phucbmt53@gmail.com>
2022-12-02 09:36:41 +0700

    update 2/12/2022

---
+++ Topic1/EventHandler/DocumentEvent/DocumentEventHandler.cs
@@ -0,0 +1,53 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic1.EventHandler.DocumentEvent
+{
+    public class DocumentEventHandler
+    {
+        private static DocumentCollection docEvents;
+
+        public void docEvents_DocumentCreated(object sender, DocumentCollectionEventArgs evtArgs)
+        {
+            Application.ShowAlertDialog(evtArgs.Document.Name + " opened!");
+        }
+
+        private static DocumentDestroyedEventHandler docDestroyedEvent;
+
+        public void OnDocumentDestroyed(object sender, DocumentDestroyedEventArgs evtArgs)
+        {
+            Application.ShowAlertDialog(evtArgs.FileName + " closed!");
+        }
+
+        [CommandMethod("StartEventHandling")]
+        public void StartEventHandling()
+        {
+            docEvents = Application.DocumentManager;
+            docEvents.DocumentCreated += new DocumentCollectionEventHandler(docEvents_DocumentCreated);
+            DocumentCollection docs = Application.DocumentManager;
+            if (docDestroyedEvent == null)
+            {
+                docDestroyedEvent = new DocumentDestroyedEventHandler(OnDocumentDestroyed);
+                docs.DocumentDestroyed += docDestroyedEvent;
+            }
+        }
+
+        [CommandMethod("TestDocCollectionEvents")]
+        public void TestDocCollectionEvents()
+        {
+            DocumentEventHandler documentEvent = new DocumentEventHandler();
+            documentEvent.Register(Application.DocumentManager);
+        }
+
+        public void Register(DocumentCollection _docEvents)
+        {
+            docEvents = _docEvents;
+            docEvents.DocumentCreated += new DocumentCollectionEventHandler(docEvents_DocumentCreated);
+        }
+    }
+}
--- Topic1/EventHandler/Objects/CircleEvent/CircleEventHandler.cs
+++ Topic1/EventHandler/Objects/CircleEvent/CircleEventHandler.cs
@@ -1,8 +1,12 @@
 ﻿using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
+using LibraryCad;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Topic1.EventHandler.Objects.CircleEvent
 {
@@ -10,8 +14,8 @@ namespace Topic1.EventHandler.Objects.CircleEvent
     {
         Circle circle = null;
 
-        [CommandMethod("AddCircleObjEvent")]
-        public void AddCircleObjEvent()
+        [CommandMethod("AddCircleModifyEvent")]
+        public void AddCircleModifyEvent()
         {
             Document doc = Application.DocumentManager.MdiActiveDocument;
             Database db = doc.Database;
@@ -28,15 +32,12 @@ namespace Topic1.EventHandler.Objects.CircleEvent
                 trans.AddNewlyCreatedDBObject(circle, true);
                 // Bắt sự kiện khi thực hiện thay đổi trên đường tròn
                 circle.Modified += new System.EventHandler(CircleMod);
-                circle.Erased += new ObjectErasedEventHandler(CircleDelete);
-                circle.Copied += new ObjectEventHandler(CircleClone);
-                circle.ModifiedXData += new System.EventHandler(CircleMod);
                 trans.Commit();
             }
         }
 
-        [CommandMethod("RemoveCircleObjEvent")]
-        public void RemoveCircleObjEvent()
+        [CommandMethod("RemoveCircleModifyEvent")]
+        public void RemoveCircleModifyEvent()
         {
             if (circle != null)
             {
@@ -50,29 +51,125 @@ namespace Topic1.EventHandler.Objects.CircleEvent
                         trans.GetObject(circle.ObjectId, OpenMode.ForWrite);
                     }
                     circle.Modified -= new System.EventHandler(CircleMod);
-                    circle.Erased -= new ObjectErasedEventHandler(CircleDelete);
-                    circle.Copied -= new ObjectEventHandler(CircleClone);
-                    circle.ModifiedXData -= new System.EventHandler(CircleMod); ;
                     circle = null;
                 }
             }
         }
 
-        public void CircleMod(object senderObj, EventArgs evtArgs)
+        [CommandMethod("AddCircleDeleteEvent")]
+        public void AddCircleDeleteEvent()
         {
-            if (circle.IsModifiedXData)
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            using (Transaction trans = db.TransactionManager.StartTransaction())
             {
-                Application.ShowAlertDialog("Xdata of " + circle.ToString() + " have changed to " + circle.XData);
-            }
-            else
-            {
-                Application.ShowAlertDialog("The area of " + circle.ToString() + " is: " + circle.Circumference);
+                // Tạo bảng để đọc và viết
+                BlockTable blockTable = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord tableRec = trans.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                circle = new Circle();
+                circle.SetDatabaseDefaults();
+                circle.Center = new Point3d(5, 5, 0);
+                circle.Radius = 5;
+                tableRec.AppendEntity(circle);
+                trans.AddNewlyCreatedDBObject(circle, true);
+                // Bắt sự kiện khi thực hiện thay đổi trên đường tròn
+                circle.Erased += new ObjectErasedEventHandler(CircleDelete);
+                trans.Commit();
             }
         }
 
-        public void CircleDelete(object senderObj, EventArgs evtArgs)
+        [CommandMethod("RemoveCircleDeleteEvent")]
+        public void RemoveCircleDeleteEvent()
         {
-            Application.ShowAlertDialog("The circle " + circle.ToString() + " have been deleted!");
+            if (circle != null)
+            {
+                Document doc = Application.DocumentManager.MdiActiveDocument;
+                Database db = doc.Database;
+                using (Transaction trans = db.TransactionManager.StartTransaction())
+                {
+                    circle = trans.GetObject(circle.ObjectId, OpenMode.ForRead) as Circle;
+                    if (circle.IsWriteEnabled == false)
+                    {
+                        trans.GetObject(circle.ObjectId, OpenMode.ForWrite);
+                    }
+                    circle.Erased -= new ObjectErasedEventHandler(CircleDelete);
+                    circle = null;
+                }
+            }
+        }
+
+        [CommandMethod("AddCircleCopyEvent")]
+        public void AddCircleCopyEvent()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                var cirId = CircleSelect();
+                if (cirId == ObjectId.Null) return;
+                circle = trans.GetObject(cirId, OpenMode.ForRead) as Circle;
+                // Bắt sự kiện khi thực hiện thay đổi trên đường tròn
+                circle.Copied += new ObjectEventHandler(CircleClone);
+                trans.Commit();
+            }
+        }
+
+        [CommandMethod("RemoveCircleCopyEvent")]
+        public void RemoveCircleCopyEvent()
+        {
+            if (circle != null)
+            {
+                Document doc = Application.DocumentManager.MdiActiveDocument;
+                Database db = doc.Database;
+                using (Transaction trans = db.TransactionManager.StartTransaction())
+                {
+                    circle = trans.GetObject(circle.ObjectId, OpenMode.ForRead) as Circle;
+                    if (circle.IsWriteEnabled == false)
+                    {
+                        trans.GetObject(circle.ObjectId, OpenMode.ForWrite);
+                    }
+                    circle.Copied -= new ObjectEventHandler(CircleClone);
+                    circle = null;
+                }
+            }
+        }
+
+        public static ObjectId CircleSelect()
+        {
+            try
+            {
+                Document doc = Application.DocumentManager.MdiActiveDocument;
+                PromptSelectionOptions prSelOptions = new PromptSelectionOptions();
+                prSelOptions.SingleOnly = true;
+                prSelOptions.SinglePickInSpace = true;
+                TypedValue[] tvCircle = new TypedValue[]
+                {
+                new TypedValue((int)DxfCode.Start, "CIRCLE")
+                };
+                SelectionFilter filter = new SelectionFilter(tvCircle);
+                PromptSelectionResult prSelResult = doc.Editor.GetSelection(prSelOptions, filter);
+                if (prSelResult.Status == PromptStatus.OK)
+                {
+                    return prSelResult.Value.OfType<SelectedObject>().First().ObjectId;
+                }
+                return ObjectId.Null;
+            }
+            catch
+            {
+                return ObjectId.Null;
+            }
+        }
+
+        public void CircleMod(object senderObj, EventArgs evtArgs)
+        {
+            Application.ShowAlertDialog("The perimeter of " + circle.ToString() + " is: " + circle.Circumference);
+            if (circle.IsErased) circle = null;
+        }
+
+        public void CircleDelete(object senderObj, ObjectErasedEventArgs evtArgs)
+        {
+            Application.ShowAlertDialog("The circle " + evtArgs.DBObject + " have been deleted!");
+            circle = null;
         }
 
         public void CircleClone(object senderObj, EventArgs evtArgs)
